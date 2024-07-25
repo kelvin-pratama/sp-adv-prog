a3af521bad944459fe8496d20b7fdaffab10f6b7
Kelvianto Pratama Harum <kelvinjuank1234@gmail.com>
2024-07-26 00:08:28 +0800

    Update KoneksiDatabase.java di P8 Project

--- P8 - Java GUI, JDBC Connector dan MySQL/Project Files/MahasiswaApp/src/program/universitas/KoneksiDatabase.java
+++ P8 - Java GUI, JDBC Connector dan MySQL/Project Files/MahasiswaApp/src/program/universitas/KoneksiDatabase.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package program.universitas;
 
-/**
- *
- * @author kelvi
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class KoneksiDatabase {
-    
+    private static Connection koneksi;
+    public static Connection getKoneksi(){
+        if(koneksi==null){
+            try{
+                String url = "jdbc:mysql://localhost/universitas";
+                String user = "root";
+                String pass = "";
+                DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url, user, pass);
+            }catch(SQLException t){
+                System.out.println("Error membuat koneksi!");
+            }
+        }
+        return koneksi;
+    }
 }
